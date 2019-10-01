888e51d5e888d70fa29de21811fbd950cf8db8be
Cody Smith <codysmith2814@gmail.com>
2019-10-01 14:43:30 -0600

    Branch

---
+++ main.js
@@ -0,0 +1,10 @@
+var photoTitle = document.querySelector("#photoTitle");
+var photoCaption = document.querySelector("#photoCaption");
+var addButton = document.querySelector("#addButton");
+var photoArray = [];
+
+addButton.addEventListener("click", instantiateCard)
+
+function instantiateCard() {
+    console.log("test")
+}
---
+++ photo.js
@@ -0,0 +1,7 @@
+class Photo {
+    constructor(id, title, caption, file)
+    this.id = Date.now();
+    this.title = title;
+    this.caption = caption;
+    this.file = file;
+}
